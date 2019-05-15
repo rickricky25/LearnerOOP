477c468227b6b6f89d164801cfee4f3cb7b4c76c
Ricky Effendi <rickricky.effendi@gmail.com>
2019-05-15 12:23:10 +0700

    Initial Commit

---
+++ LearnerOOP/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LearnerOOP
+//
+//  Created by Ricky Effendi on 15/05/19.
+//  Copyright © 2019 Ricky Effendi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
